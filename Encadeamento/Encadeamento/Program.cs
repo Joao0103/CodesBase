2d737a0f76b08d53c5b05e5adaf3b2ab34633b75
João Marques <jvmarquesrodrigues3@gmail.com>
2022-03-22 17:27:44 -0300

    Uptade

---
+++ Encadeamento/Encadeamento/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Encadeamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X, Y, Z;
+            string[] vet = Console.ReadLine().Split(' ');
+
+            X = int.Parse(vet[0]);
+            Y = int.Parse(vet[1]);
+            Z = int.Parse(vet[2]);
+
+            if (X < Y && X < Z)
+            {
+                Console.WriteLine("MENOR = " + X);
+
+            }
+            else
+            {
+                if (Y < Z)
+                {
+                    Console.WriteLine("MENOR = " + Y);
+                }
+                else
+                {
+                    Console.WriteLine("MENOR = " + Z);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EncadeamentoDeEstruturasCondicionais/EncadeamentoDeEstruturasCondicionais/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace EncadeamentoDeEstruturasCondicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int horas;
+
+            Console.WriteLine("Quantas horas? ");
+            horas = int.Parse(Console.ReadLine());
+
+            if (horas < 12)
+            {
+                Console.WriteLine("Bom dia! ");
+            }
+            else
+            {
+                if (horas < 18) 
+                {
+                    Console.WriteLine("Boa tarde! ");
+                }
+                else
+                {
+                    Console.WriteLine("Boa noite! ");
+                }
+            }
+            
+            
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
