5d2f231505248220d95c1f2e26b7d39a3b8f92e6
João Marques <jvmarquesrodrigues3@gmail.com>
2022-03-22 17:28:38 -0300

    Uptade

---
+++ Uri 1035/Uri 1035/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Uri_1035
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] valores = Console.ReadLine().Split(' ');
+
+            int a = int.Parse(valores[0]);
+            int b = int.Parse(valores[1]);
+            int c = int.Parse(valores[2]);
+            int d = int.Parse(valores[3]);
+
+            if (b > c && d > a && c + d > a + b && c > 0 && d > 0 && a % 2 == 0)
+            {
+                Console.WriteLine("Valores aceitos");
+            }
+            else
+            {
+                Console.WriteLine("Valores não aceitos");
+            }
+
+        }
+
+    }
+}
+
+
