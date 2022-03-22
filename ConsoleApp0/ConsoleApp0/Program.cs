b460a2d346f0ebdf639522cd364cd6980e357742
João Marques <jvmarquesrodrigues3@gmail.com>
2022-03-22 17:25:59 -0300

    Uptade

---
+++ ConsoleApp0/ConsoleApp0/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace uri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = int.Parse(Console.ReadLine());
+
+            if (m == 0 || m == 45 || m == 360)
+            {
+                Console.WriteLine("Bom Dia!!");
+            }
+            if (m == 90)
+            {
+                Console.WriteLine("Boa Tarde!!");
+            }
+            if (m == 180)
+            {
+                Console.WriteLine("Boa Noite!!");
+            }
+            else
+            {
+                Console.WriteLine("De Madrugada!!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
