aac8217b393d0a5e84d9af9a400d4fc4d9c8f77f
João Marques <jvmarquesrodrigues3@gmail.com>
2022-03-22 17:27:20 -0300

    Uptade

---
+++ AtividadeDoURIOnline/AtividadeDoURIOnline/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri1011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            double Raio, volume;
+           
+
+
+            Raio = double.Parse(Console.ReadLine());
+            volume = 4.0 / 3.0 * 3.14159 * Raio * Raio * Raio;
+            Console.WriteLine("VOLUME = " + volume.ToString("F3", CultureInfo.InvariantCulture));
+
+
+            Console.ReadLine();
+           
+          
+        }
+    }
+}
